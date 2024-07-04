4b9f1c03f9267872d96c07067259ecad98e19146
Lucas Francisco <lucasarnoldd@gmail.com>
2024-07-03 22:15:09 -0300

    :recycle: refactor(Env): Refactor environment vars

--- database/index.ts
+++ database/index.ts
@@ -3,10 +3,11 @@ import { knex as setupKnex, Knex } from 'knex'
 
 
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
+  client: env.DATABASE_CLIENT,
+
+  connection: env.DATABASE_CLIENT ===  'sqlite' ? {
     filename: env.DATABASE_URL,
-  },
+  } : env.DATABASE_URL,
 
   migrations: {
     extension: 'ts',
